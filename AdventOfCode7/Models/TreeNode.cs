fa5595b0b3da46b3d1c836cf016fd3a6e9dd74ec
Christopher Sanborn <CPJSanborn@juno.com>
2018-12-08 08:05:21 -0500

    Trying to build custom tree

---
+++ AdventOfCode7/Models/TreeNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode7.Models
+{
+    public class TreeNode
+    {
+        public char Name { get; set; }
+
+        public List<TreeNode> ChildNodes { get; set; } = new List<TreeNode>();
+
+        public TreeNode(char name)
+        {
+            Name = name;
+        }
+    }
+}
--- AdventOfCode7/Program.cs
+++ AdventOfCode7/Program.cs
@@ -51,17 +51,44 @@ namespace AdventOfCode7
             Console.WriteLine("Beginning: " + beginning.Name);
 
             //Find ending
-            char noNameNode = ' ';
+            char ending = ' ';
             char findMe = ' ';
             for (int i = 0; i < listOfNodes.Count; i++)
             {
                 findMe = listOfNodes[i].DependsOn;
                 if (listOfNodes.All(x => x.Name != findMe))
                 {
-                    noNameNode = findMe;
+                    ending = findMe;
                 }
             }            
-            Console.WriteLine("ending: " + noNameNode);
+            Console.WriteLine("ending: " + ending);
+
+            // Now let's build the list
+            //linkedListNodes.AddFirst(beginning);
+            //linkedListNodes.AddLast(ending);
+
+            TreeNode myTree = new TreeNode(beginning.Name);
+          
+            foreach(var c in listOfNodes)
+{
+                if (c.Name == myTree.Name)
+                    {
+                    var newNode = new TreeNode(c.DependsOn);
+                    myTree.ChildNodes.Add(newNode);
+}
+                else
+                    {
+                    var currentNode = myTree;
+                    var childNode = recursiveNodeFind(c, myTree);
+                    if (childNode != null)
+                    {
+                        childNode.ChildNodes.Add(new TreeNode(c.Name));
+                    }
+                }
+            }
+
+            //Display
+            DisplayTree(myTree);
 
             // Part II
             int partTwoAnswer = 0;
@@ -74,6 +101,46 @@ namespace AdventOfCode7
             Console.WriteLine("******************");
             Console.WriteLine("Press any key to end...");
             Console.ReadLine();
+        }
+
+
+        private static TreeNode recursiveNodeFind(Node inNode, TreeNode tree)
+        {
+             if (tree.Name == inNode.Name)
+                return tree;
+             else 
+             {
+                var foundNode = tree.ChildNodes.FirstOrDefault(x => x.Name == inNode.Name);
+                bool isFoundNode = false;
+                foreach(var cn in tree.ChildNodes)
+                {
+                    if (isFoundNode)
+                        break;
+                    var childNodeFind = recursiveNodeFind(inNode, cn);
+                    if (childNodeFind != null)
+                    {
+                        isFoundNode = true;
+                        return childNodeFind;
+                        break;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                if (!isFoundNode)
+                    return tree;
+             }
+            return null;
+        }
+
+        private static void DisplayTree(TreeNode myTree)
+        {
+            Console.Write(myTree.Name);
+            foreach(var node in myTree.ChildNodes.OrderBy(x => x.Name))
+            {
+                DisplayTree(node);
+            }
         }
     }
 }
