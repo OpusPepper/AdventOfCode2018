f32b62c391df7f2d0d00f557c2db8e7eb3f00e11
Christopher Sanborn <CPJSanborn@juno.com>
2018-12-11 17:13:28 -0500

    Day 8, Part I working

---
+++ AdventOfCode8/Models/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode8.Models
+{
+    public class Node
+    {
+        public Tuple<int, int> Header { get; set; }
+
+        public List<Node> ChildNodes { get; set; } = new List<Node>();
+
+        public List<int> MetaData { get; set; } = new List<int>();
+
+        public Node(Tuple<int, int> tupleIn)
+        {
+            Header = tupleIn;
+        }
+    }
+}
---
+++ AdventOfCode8/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using AdventOfCode8.Models;
+
+namespace AdventOfCode8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part I
+            string path = Path.Combine(@"..\..\Data\input.txt");
+            string[] allLines = File.ReadAllLines(path);
+            int partOneAnswer = 0;
+            string delimited = @"(\d+)";
+            List<int> inputNumbers = new List<int>();
+            List<Node> allNodes = new List<Node>();
+            int counter = 0;
+
+            // Find beginning and end nodes
+            foreach (var line in allLines)
+            {
+                var matches = Regex.Matches(line, delimited);
+
+                foreach (Match m in matches)
+                {
+                    inputNumbers.Add(Convert.ToInt32(m.Value));
+                }
+            }
+
+            Node newNode;
+
+            newNode = getNode(inputNumbers);
+
+            partOneAnswer = getTotalOfMetaData(newNode);
+
+            // Results
+            Console.WriteLine("******************");
+            Console.WriteLine("AdventOfCode Day 8");
+            Console.WriteLine("Part I: " + partOneAnswer);
+            Console.WriteLine("Part II: " + "");
+            Console.WriteLine("******************");
+            Console.WriteLine("Press any key to end...");
+            Console.ReadLine();
+        }
+
+        private static Node getNode(List<int> inList)
+        {
+            Node returnValue = null;
+
+            if (inList.Any())
+            {
+                returnValue = new Node(new Tuple<int, int>(inList[0], inList[1]));
+                inList.RemoveRange(0, 2);
+
+                if (returnValue.Header.Item1 > 0)
+                {
+                    // Check if there is a childnode
+                    for (int i = 0; i < returnValue.Header.Item1; i++)
+                    {                        
+                        returnValue.ChildNodes.Add(getNode(inList));
+                    }
+                }
+
+                if (returnValue.Header.Item2 > 0)
+                {
+                    for (int i = 0; i < returnValue.Header.Item2; i++)
+                    {
+                        returnValue.MetaData.Add(inList[i]);
+                    }
+                    inList.RemoveRange(0, returnValue.Header.Item2);
+                }
+
+            }
+
+        return returnValue;
+        }
+
+        private static int getTotalOfMetaData(Node inNode)
+        {
+            int returnValue = 0;
+
+            foreach (var i in inNode.MetaData)
+            {
+                returnValue += i;
+            }
+
+            if (inNode.ChildNodes.Any())
+            {
+                foreach (var cn in inNode.ChildNodes)
+                {
+                    returnValue += getTotalOfMetaData(cn);
+                }                
+            }
+
+            return returnValue;
+        }
+
+    }
+}
