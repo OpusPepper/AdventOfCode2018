1a553f3c7049e83a32b3a3b6a6390158f37bec94
OpusPepper <45573657+OpusPepper@users.noreply.github.com>
2018-12-20 16:19:54 -0500

    Day 14 working examples

---
+++ AdventOfCode14/Models/Elf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode14.Models
+{
+    public class Elf
+    {
+        public int ElfId { get; set; }
+        public char EnclosingBracketBegin { get; set; }
+        public char EnclosingBracketEnd { get; set; }
+        public int CurrentRecipeIndex { get; set; }
+
+        public Elf(int elfId, char enclosingBracketBegin, char enclosingBracketEnd, int currentRecipeIndex)
+        {
+            ElfId = elfId;
+            EnclosingBracketBegin = enclosingBracketBegin;
+            EnclosingBracketEnd = enclosingBracketEnd;
+            CurrentRecipeIndex = currentRecipeIndex;
+        }
+    }
+}
---
+++ AdventOfCode14/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode14.Models
+{
+    public class Recipe
+    {
+        public int RecipeScore { get; set; }
+
+        public bool ElfCurrentlyWorking { get; set; }
+
+        public int IdOfElfCurrentlyWorking { get; set; }
+
+        public Recipe(int recipeScore, int idOfElfCurrentlyWorking)
+        {
+            RecipeScore = recipeScore;
+            IdOfElfCurrentlyWorking = idOfElfCurrentlyWorking;
+        }
+    }
+}
